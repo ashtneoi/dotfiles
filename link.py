c1d50d9df738f3672c07148f080e990d2293f411
ashtneoi <ashtneoi@gmail.com>
2014-09-04 14:04:46 -0700

    Update to dotfile-manager 2eb1486

--- link.py
+++ link.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python2
 
 from __future__ import unicode_literals
-from os import chdir, path, symlink
+
+import os
+from os import path
 from sys import stdout, stderr
 
 from file_list import file_list
 
 
 def main(dry_run):
-    chdir('files')
+    os.chdir('files')
     for name, directory in file_list.iteritems():
         directory = path.expanduser(directory)
+        if not os.access(directory, os.W_OK):
+            os.makedirs(directory)
         target = path.relpath(name, directory)
         link_name = path.abspath(path.join(directory, name))
         stdout.write('{} --> {}\n'.format(target, link_name))
         if not dry_run:
             try:
-                symlink(target, link_name)
+                os.symlink(target, link_name)
             except OSError as e:
                 if e.errno != 17:  # File exists
                     raise
